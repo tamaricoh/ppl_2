f2ecba7a749be4c37f7b152100ee3ac2edb5a21b
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2024-06-15 17:58:55 +0300

    base for 2b

--- L3/L3-eval-sub.ts
+++ L3/L3-eval-sub.ts
@@ -1,17 +1,74 @@
 // L3-eval.ts
 import { map } from "ramda";
-import { ClassExp, isCExp, isLetExp, isClassExp,makeClassExp, Binding, isBinding } from "./L3-ast";
-import { BoolExp, CExp, Exp, IfExp, LitExp, NumExp,
-         PrimOp, ProcExp, Program, StrExp, VarDecl } from "./L3-ast";
-import { isAppExp, isBoolExp, isDefineExp, isIfExp, isLitExp, isNumExp,
-             isPrimOp, isProcExp, isStrExp, isVarRef } from "./L3-ast";
-import { makeBoolExp, makeLitExp, makeNumExp, makeProcExp, makeStrExp } from "./L3-ast";
+import { isCExp, isLetExp } from "./L3-ast";
+import {
+  BoolExp,
+  CExp,
+  Exp,
+  IfExp,
+  LitExp,
+  NumExp,
+  PrimOp,
+  ProcExp,
+  Program,
+  StrExp,
+  VarDecl,
+  ClassExp,
+  Binding,
+} from "./L3-ast";
+import {
+  isAppExp,
+  isBoolExp,
+  isDefineExp,
+  isIfExp,
+  isLitExp,
+  isNumExp,
+  isPrimOp,
+  isProcExp,
+  isStrExp,
+  isVarRef,
+  isClassExp,
+  isBinding,
+} from "./L3-ast";
+import {
+  makeBoolExp,
+  makeLitExp,
+  makeNumExp,
+  makeProcExp,
+  makeStrExp,
+  makeClassExp,
+} from "./L3-ast";
 import { parseL3Exp } from "./L3-ast";
 import { applyEnv, makeEmptyEnv, makeEnv, Env } from "./L3-env-sub";
-import { isClosure, makeClosure, Closure, Value } from "./L3-value";
-import { first, rest, isEmpty, List, isNonEmptyList, allT } from '../shared/list';
+import {
+  isClosure,
+  makeClosure,
+  Closure,
+  isClass,
+  makeClass,
+  Class,
+  isObject,
+  makeObject,
+  Object,
+  Value,
+} from "./L3-value";
+import {
+  first,
+  rest,
+  isEmpty,
+  List,
+  isNonEmptyList,
+  allT,
+} from "../shared/list";
 import { isBoolean, isNumber, isString } from "../shared/type-predicates";
-import { Result, makeOk, makeFailure, bind, mapResult, mapv } from "../shared/result";
+import {
+  Result,
+  makeOk,
+  makeFailure,
+  bind,
+  mapResult,
+  mapv,
+} from "../shared/result";
 import { renameExps, substitute } from "./substitute";
 import { applyPrimitive } from "./evalPrimitive";
 import { parse as p } from "../shared/parser";
@@ -22,99 +79,155 @@ import { format } from "../shared/format";
 // Eval functions
 
 const L3applicativeEval = (exp: CExp, env: Env): Result<Value> =>
-    isNumExp(exp) ? makeOk(exp.val) : 
-    isBoolExp(exp) ? makeOk(exp.val) :
-    isStrExp(exp) ? makeOk(exp.val) :
-    isPrimOp(exp) ? makeOk(exp) :
-    isVarRef(exp) ? applyEnv(env, exp.var) :
-    isLitExp(exp) ? makeOk(exp.val) :
-    isIfExp(exp) ? evalIf(exp, env) :
-    isProcExp(exp) ? evalProc(exp, env) :
-    isAppExp(exp) ? bind(L3applicativeEval(exp.rator, env), (rator: Value) =>
-                        bind(mapResult(param => 
-                            L3applicativeEval(param, env), 
-                              exp.rands), 
-                            (rands: Value[]) =>
-                                L3applyProcedure(rator, rands, env))) :
-    isLetExp(exp) ? makeFailure('"let" not supported (yet)') :
-    isClassExp(exp)? evalClass(exp,env):
-    makeFailure('Never');
+  isNumExp(exp)
+    ? makeOk(exp.val)
+    : isBoolExp(exp)
+    ? makeOk(exp.val)
+    : isStrExp(exp)
+    ? makeOk(exp.val)
+    : isPrimOp(exp)
+    ? makeOk(exp)
+    : isVarRef(exp)
+    ? applyEnv(env, exp.var)
+    : isLitExp(exp)
+    ? makeOk(exp.val)
+    : isIfExp(exp)
+    ? evalIf(exp, env)
+    : isProcExp(exp)
+    ? evalProc(exp, env)
+    : isAppExp(exp)
+    ? bind(L3applicativeEval(exp.rator, env), (rator: Value) =>
+        bind(
+          mapResult((param) => L3applicativeEval(param, env), exp.rands),
+          (rands: Value[]) => L3applyProcedure(rator, rands, env)
+        )
+      )
+    : isLetExp(exp)
+    ? makeFailure('"let" not supported (yet)')
+    : //================================================
+    isClassExp(exp)
+    ? evalClass(exp, env)
+    : makeFailure("Never");
 
-export const isTrueValue = (x: Value): boolean =>
-    ! (x === false);
+export const isTrueValue = (x: Value): boolean => !(x === false);
 
 const evalIf = (exp: IfExp, env: Env): Result<Value> =>
-    bind(L3applicativeEval(exp.test, env), (test: Value) => 
-        isTrueValue(test) ? L3applicativeEval(exp.then, env) : 
-        L3applicativeEval(exp.alt, env));
+  bind(L3applicativeEval(exp.test, env), (test: Value) =>
+    isTrueValue(test)
+      ? L3applicativeEval(exp.then, env)
+      : L3applicativeEval(exp.alt, env)
+  );
 
 const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>
-    makeOk(makeClosure(exp.args, exp.body));
+  makeOk(makeClosure(exp.args, exp.body));
 
-const L3applyProcedure = (proc: Value, args: Value[], env: Env): Result<Value> =>
-    isPrimOp(proc) ? applyPrimitive(proc, args) :
-    isClosure(proc) ? applyClosure(proc, args, env) :
-    makeFailure(`Bad procedure ${format(proc)}`);
+const L3applyProcedure = (
+  proc: Value,
+  args: Value[],
+  env: Env
+): Result<Value> =>
+  isPrimOp(proc)
+    ? applyPrimitive(proc, args)
+    : isClosure(proc)
+    ? applyClosure(proc, args, env)
+    : //================================================
+    isClass(proc)
+    ? applyClass(proc, args)
+    : isObject(proc)
+    ? applyObject(proc)
+    : makeFailure(`Bad procedure ${format(proc)}`);
 
 // Applications are computed by substituting computed
 // values into the body of the closure.
 // To make the types fit - computed values of params must be
 // turned back in Literal Expressions that eval to the computed value.
-const valueToLitExp = (v: Value): NumExp | BoolExp | StrExp | LitExp | PrimOp | ProcExp | ClassExp=>
-    isNumber(v) ? makeNumExp(v) :
-    isBoolean(v) ? makeBoolExp(v) :
-    isString(v) ? makeStrExp(v) :
-    isPrimOp(v) ? v :
-    isClosure(v) ? makeProcExp(v.params, v.body) :
-    isClassExp(v)? makeClassExp(v.fields, v.methods): 
-    makeLitExp(v);
+const valueToLitExp = (
+  v: Value
+): NumExp | BoolExp | StrExp | LitExp | PrimOp | ProcExp => // | ClassExp
+  isNumber(v)
+    ? makeNumExp(v)
+    : isBoolean(v)
+    ? makeBoolExp(v)
+    : isString(v)
+    ? makeStrExp(v)
+    : isPrimOp(v)
+    ? v
+    : isClosure(v)
+    ? makeProcExp(v.params, v.body)
+    : // : isClassExp(v)
+      // ? makeClassExp(v.fields, v.methods)
+      makeLitExp(v);
 
-const applyClosure = (proc: Closure, args: Value[], env: Env): Result<Value> => {
-    const vars = map((v: VarDecl) => v.var, proc.params);
-    const body = renameExps(proc.body);
-    const litArgs : CExp[] = map(valueToLitExp, args);
-    return evalSequence(substitute(body, vars, litArgs), env);
-    //return evalSequence(substitute(proc.body, vars, litArgs), env);
-}
-//------b2
-export const evalClass = (exp: ClassExp, env: Env): Result<Value> =>{
-const fields = map((b:VarDecl)=> b.var, exp.fields);
-const methods = exp.methods;
-if (allT(isBinding, methods)) {
-    const paramss = map((v: ProcExp) => v.args, vals);
-    const bodies = map((v: ProcExp) => v.body, vals);
-    return evalSequence(substitute(exp.methods,fields,exp.methods), makeRecEnv(vars, paramss, bodies, env));
-} else {
-    return makeFailure(`Letrec: all variables must be bound to procedures: ${format(exp)}`);
-}};
+const applyClosure = (
+  proc: Closure,
+  args: Value[],
+  env: Env
+): Result<Value> => {
+  const vars = map((v: VarDecl) => v.var, proc.params);
+  const body = renameExps(proc.body);
+  const litArgs: CExp[] = map(valueToLitExp, args);
+  return evalSequence(substitute(body, vars, litArgs), env);
+  //return evalSequence(substitute(proc.body, vars, litArgs), env);
+};
+
+//================================================
+const applyClass = (proc: Class, args: Value[]): Result<Value> =>
+  makeOk(makeObject(proc, args));
+
+//================================================
+const applyObject = (proc: Object): Result<Value> => {
+  return makeFailure(""); //------------------------------------------------------
+};
+
+//================================================
+export const evalClass = (exp: ClassExp, env: Env): Result<Value> => {
+  //   return makeOk(makeClass()); //--------------------------------------------------------
+  return makeFailure("");
+  //   const fields = map((b: VarDecl) => b.var, exp.fields);
+  //   const methods = exp.methods;
+  //   if (allT(isBinding, methods)) {
+  //     const paramss = map((v: ProcExp) => v.args, vals);
+  //     const bodies = map((v: ProcExp) => v.body, vals);
+  //     return evalSequence(
+  //       substitute(exp.methods, fields, exp.methods),
+  //       makeRecEnv(vars, paramss, bodies, env)
+  //     );
+  //   } else {
+  //     return makeFailure(
+  //       `Letrec: all variables must be bound to procedures: ${format(exp)}`
+  //     );
+  //   }
+};
 // Evaluate a sequence of expressions (in a program)
 export const evalSequence = (seq: List<Exp>, env: Env): Result<Value> =>
-    isNonEmptyList<Exp>(seq) ? 
-        isDefineExp(first(seq)) ? evalDefineExps(first(seq), rest(seq), env) :
-        evalCExps(first(seq), rest(seq), env) :
-    makeFailure("Empty sequence");
+  isNonEmptyList<Exp>(seq)
+    ? isDefineExp(first(seq))
+      ? evalDefineExps(first(seq), rest(seq), env)
+      : evalCExps(first(seq), rest(seq), env)
+    : makeFailure("Empty sequence");
 
 const evalCExps = (first: Exp, rest: Exp[], env: Env): Result<Value> =>
-    isCExp(first) && isEmpty(rest) ? L3applicativeEval(first, env) :
-    isCExp(first) ? bind(L3applicativeEval(first, env), _ => 
-                            evalSequence(rest, env)) :
-    makeFailure("Never");
+  isCExp(first) && isEmpty(rest)
+    ? L3applicativeEval(first, env)
+    : isCExp(first)
+    ? bind(L3applicativeEval(first, env), (_) => evalSequence(rest, env))
+    : makeFailure("Never");
 
 // Eval a sequence of expressions when the first exp is a Define.
 // Compute the rhs of the define, extend the env with the new binding
 // then compute the rest of the exps in the new env.
 const evalDefineExps = (def: Exp, exps: Exp[], env: Env): Result<Value> =>
-    isDefineExp(def) ? bind(L3applicativeEval(def.val, env), 
-                            (rhs: Value) => 
-                                evalSequence(exps, 
-                                    makeEnv(def.var.var, rhs, env))) :
-    makeFailure(`Unexpected in evalDefine: ${format(def)}`);
+  isDefineExp(def)
+    ? bind(L3applicativeEval(def.val, env), (rhs: Value) =>
+        evalSequence(exps, makeEnv(def.var.var, rhs, env))
+      )
+    : makeFailure(`Unexpected in evalDefine: ${format(def)}`);
 
 // Main program
 export const evalL3program = (program: Program): Result<Value> =>
-    evalSequence(program.exps, makeEmptyEnv());
+  evalSequence(program.exps, makeEmptyEnv());
 
 export const evalParse = (s: string): Result<Value> =>
-    bind(p(s), (sexp: Sexp) => 
-        bind(parseL3Exp(sexp), (exp: Exp) =>
-            evalSequence([exp], makeEmptyEnv())));
+  bind(p(s), (sexp: Sexp) =>
+    bind(parseL3Exp(sexp), (exp: Exp) => evalSequence([exp], makeEmptyEnv()))
+  );
--- L3/L3-value.ts
+++ L3/L3-value.ts
@@ -1,118 +1,195 @@
 // ========================================================
 // Value type definition for L4
 
-import { isPrimOp, CExp, PrimOp, VarDecl ,Binding, ClassExp, isClassExp} from './L3-ast';
-import { Env, makeEmptyEnv } from './L3-env-env';
-import { append } from 'ramda';
-import { isArray, isNumber, isString } from '../shared/type-predicates';
+import {
+  isPrimOp,
+  CExp,
+  PrimOp,
+  VarDecl,
+  Binding,
+  ClassExp,
+  isClassExp,
+} from "./L3-ast";
+import { Env, makeEmptyEnv } from "./L3-env-env";
+import { append } from "ramda";
+import { isArray, isNumber, isString } from "../shared/type-predicates";
 
-
-export type Value = SExpValue | ClassExp;
+export type Value = SExpValue;
 
 export type Functional = PrimOp | Closure;
-export const isFunctional = (x: any): x is Functional => isPrimOp(x) || isClosure(x);
+export const isFunctional = (x: any): x is Functional =>
+  isPrimOp(x) || isClosure(x);
 
 // ========================================================
 // Closure for L4 - the field env is added.
 // We also use a frame-based representation of closures as opposed to one env per var.
 export type Closure = {
-    tag: "Closure";
-    params: VarDecl[];
-    body: CExp[];
-    env: Env;
-}
-export const makeClosure = (params: VarDecl[], body: CExp[]): Closure =>
-    ({tag: "Closure", params: params, body: body, env : makeEmptyEnv()});
-export const makeClosureEnv = (params: VarDecl[], body: CExp[], env: Env): Closure =>
-    ({tag: "Closure", params: params, body: body, env: env});
+  tag: "Closure";
+  params: VarDecl[];
+  body: CExp[];
+  env: Env;
+};
+export const makeClosure = (params: VarDecl[], body: CExp[]): Closure => ({
+  tag: "Closure",
+  params: params,
+  body: body,
+  env: makeEmptyEnv(),
+});
+export const makeClosureEnv = (
+  params: VarDecl[],
+  body: CExp[],
+  env: Env
+): Closure => ({ tag: "Closure", params: params, body: body, env: env });
 export const isClosure = (x: any): x is Closure => x.tag === "Closure";
 
 // ========================================================
-//------------- b2
-// export type Class = {
-//     tag: "Class";
-//     fields: VarDecl[];
-//     methods: Binding[];
-//     env: Env;
-// }
+// ------------- b2
+export type Class = {
+  // thats not ClassExp!!!
+  tag: "Class";
+  fields: VarDecl[];
+  methods: Binding[];
+  env: Env;
+};
 
-// export const makeClass= (fields: VarDecl[],methods: Binding[]): ClassExp =>
-//     ({tag: "ClassExp", fields: fields, methods: methods});
-// export const makeClassEnv= (fields: VarDecl[],methods: Binding[], env:Env): ClassExp =>
-//     ({tag: "ClassExp", fields: fields, methods: methods});
-// export const isClass = (x: any): x is ClassExp => x.tag === "ClassExp";
+export const makeClass = (fields: VarDecl[], methods: Binding[]): Class => ({
+  tag: "Class",
+  fields: fields,
+  methods: methods,
+  env: makeEmptyEnv(),
+});
+export const makeClassEnv = (
+  fields: VarDecl[],
+  methods: Binding[],
+  env: Env
+): Class => ({ tag: "Class", fields: fields, methods: methods, env: env });
+export const isClass = (x: any): x is Class => x.tag === "Class";
 
 export type Object = {
-    tag: "Object";
-    fields: VarDecl[];
-    methods: Binding[];
-    env: Env;
-}
+  tag: "Object";
+  class: Class;
+  args: Value[];
+  env: Env;
+};
 
-export const makeObject= (fields: VarDecl[],methods: Binding[]): Object =>
-    ({tag: "Object", fields: fields, methods: methods, env : makeEmptyEnv()});
-export const makeObjectEnv= (fields: VarDecl[],methods: Binding[], env:Env): Object =>
-    ({tag: "Object", fields: fields, methods: methods, env: env});
+export const makeObject = (clas: Class, arg: Value[]): Object => ({
+  tag: "Object",
+  class: clas,
+  args: arg,
+  env: makeEmptyEnv(),
+});
+export const makeObjectEnv = (clas: Class, arg: Value[], env: Env): Object => ({
+  tag: "Object",
+  class: clas,
+  args: arg,
+  env: env,
+});
 export const isObject = (x: any): x is Object => x.tag === "Object";
 //-------------b2
+
 // SExp
 export type CompoundSExp = {
-    tag: "CompoundSexp";
-    val1: SExpValue;
-    val2: SExpValue;
-}
+  tag: "CompoundSexp";
+  val1: SExpValue;
+  val2: SExpValue;
+};
 export type EmptySExp = {
-    tag: "EmptySExp";
-}
+  tag: "EmptySExp";
+};
 export type SymbolSExp = {
-    tag: "SymbolSExp";
-    val: string;
-}
+  tag: "SymbolSExp";
+  val: string;
+};
 
-export type SExpValue = number | boolean | string | PrimOp | Closure | SymbolSExp | EmptySExp | CompoundSExp;
+export type SExpValue =
+  | number
+  | boolean
+  | string
+  | PrimOp
+  | Closure
+  | SymbolSExp
+  | EmptySExp
+  | CompoundSExp
+  | Class
+  | Object;
 export const isSExp = (x: any): x is SExpValue =>
-    typeof(x) === 'string' || typeof(x) === 'boolean' || typeof(x) === 'number' ||
-    isSymbolSExp(x) || isCompoundSExp(x) || isEmptySExp(x) || isPrimOp(x) || isClosure(x);
+  typeof x === "string" ||
+  typeof x === "boolean" ||
+  typeof x === "number" ||
+  isSymbolSExp(x) ||
+  isCompoundSExp(x) ||
+  isEmptySExp(x) ||
+  isPrimOp(x) ||
+  isClosure(x);
 
-export const makeCompoundSExp = (val1: SExpValue, val2: SExpValue): CompoundSExp =>
-    ({tag: "CompoundSexp", val1: val1, val2 : val2});
-export const isCompoundSExp = (x: any): x is CompoundSExp => x.tag === "CompoundSexp";
+export const makeCompoundSExp = (
+  val1: SExpValue,
+  val2: SExpValue
+): CompoundSExp => ({ tag: "CompoundSexp", val1: val1, val2: val2 });
+export const isCompoundSExp = (x: any): x is CompoundSExp =>
+  x.tag === "CompoundSexp";
 
-export const makeEmptySExp = (): EmptySExp => ({tag: "EmptySExp"});
+export const makeEmptySExp = (): EmptySExp => ({ tag: "EmptySExp" });
 export const isEmptySExp = (x: any): x is EmptySExp => x.tag === "EmptySExp";
 
-export const makeSymbolSExp = (val: string): SymbolSExp =>
-    ({tag: "SymbolSExp", val: val});
+export const makeSymbolSExp = (val: string): SymbolSExp => ({
+  tag: "SymbolSExp",
+  val: val,
+});
 export const isSymbolSExp = (x: any): x is SymbolSExp => x.tag === "SymbolSExp";
 
 // LitSExp are equivalent to JSON - they can be parsed and read as literal values
 // like SExp except that non functional values (PrimOp and Closures) can be embedded at any level.
-export type LitSExp = number | boolean | string | SymbolSExp | EmptySExp | CompoundSExp;
+export type LitSExp =
+  | number
+  | boolean
+  | string
+  | SymbolSExp
+  | EmptySExp
+  | CompoundSExp;
 
 // Printable form for values
 export const closureToString = (c: Closure): string =>
-    // `<Closure ${c.params} ${L3unparse(c.body)}>`
-    `<Closure ${c.params} ${c.body}>`
+  // `<Closure ${c.params} ${L3unparse(c.body)}>`
+  `<Closure ${c.params} ${c.body}>`;
 
-export const compoundSExpToArray = (cs: CompoundSExp, res: string[]): string[] | { s1: string[], s2: string } =>
-    isEmptySExp(cs.val2) ? append(valueToString(cs.val1), res) :
-    isCompoundSExp(cs.val2) ? compoundSExpToArray(cs.val2, append(valueToString(cs.val1), res)) :
-    ({ s1: append(valueToString(cs.val1), res), s2: valueToString(cs.val2)})
- 
-export const compoundSExpToString = (cs: CompoundSExp, css = compoundSExpToArray(cs, [])): string => 
-    isArray(css) ? `(${css.join(' ')})` :
-    `(${css.s1.join(' ')} . ${css.s2})`
+export const compoundSExpToArray = (
+  cs: CompoundSExp,
+  res: string[]
+): string[] | { s1: string[]; s2: string } =>
+  isEmptySExp(cs.val2)
+    ? append(valueToString(cs.val1), res)
+    : isCompoundSExp(cs.val2)
+    ? compoundSExpToArray(cs.val2, append(valueToString(cs.val1), res))
+    : { s1: append(valueToString(cs.val1), res), s2: valueToString(cs.val2) };
+
+export const compoundSExpToString = (
+  cs: CompoundSExp,
+  css = compoundSExpToArray(cs, [])
+): string =>
+  isArray(css) ? `(${css.join(" ")})` : `(${css.s1.join(" ")} . ${css.s2})`;
 
 export const valueToString = (val: Value): string =>
-    isNumber(val) ?  val.toString() :
-    val === true ? '#t' :
-    val === false ? '#f' :
-    isString(val) ? `"${val}"` :
-    isClosure(val) ? closureToString(val) :
-    isPrimOp(val) ? val.op :
-    isSymbolSExp(val) ? val.val :
-    isEmptySExp(val) ? "'()" :
-    isCompoundSExp(val) ? compoundSExpToString(val) :
-    isClassExp(val)? " "://-------------b2
-    isObject(val)? " "://-------------b2
-    val;
+  isNumber(val)
+    ? val.toString()
+    : val === true
+    ? "#t"
+    : val === false
+    ? "#f"
+    : isString(val)
+    ? `"${val}"`
+    : isClosure(val)
+    ? closureToString(val)
+    : isPrimOp(val)
+    ? val.op
+    : isSymbolSExp(val)
+    ? val.val
+    : isEmptySExp(val)
+    ? "'()"
+    : isCompoundSExp(val)
+    ? compoundSExpToString(val)
+    : isClass(val)
+    ? " " //-------------------------------------------------
+    : isObject(val)
+    ? " " //-------------------------------------------------
+    : val;
